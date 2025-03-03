70c7a88b8789dc3272f955ff517722dd4a804fe5
Kot <huangkatherine7@gmail.com>
2025-03-02 21:32:35 -0500

    female stats

--- stats/f_friedman.py
+++ stats/f_friedman.py
@@ -5,23 +5,27 @@ from scipy.stats import wilcoxon
 
 #6 guys - stat sig for friedman 
 #8 guys - stat sig for paired (can include or not include the guy who put 1s for everything)
-above_f = [1, 2, 3, 5, 3, 3, 2, 2, 5] #only first 5 are real data points
-eye_f = [2, 3, 2, 2, 3, 2, 3, 1, 2]
-right_f = [4, 7, 1, 6, 1, 1, 3, 1, 4]
-below_f = [6, 7, 6, 5, 7, 7, 6, 1, 7]
+above_f = [5, 3, 3, 6, 5, 3, 3, 6] #only first 4 are real data points
+eye_f = [5, 1, 4, 2, 5, 1, 4, 2]
+right_f = [4, 2, 3, 4, 4, 2, 3, 4]
+below_f = [6, 2, 6, 6, 6, 2, 6, 6]
 
 # Running the Friedman test
 statistic_f, p_value_f = friedmanchisquare(above_f, eye_f, right_f, below_f)
 
 # Printing results with labels
-print(f"MALE Friedman test statistic: {statistic_f}")
-print(f"MALE Friedman test p-value: {p_value_f}")
+print(f"FEMALE Friedman test statistic: {statistic_f}")
+print(f"FEMALE Friedman test p-value: {p_value_f}")
 print("Stat sig: YES\n\n" if p_value_f < 0.05 else "Stat sig: NO\n\n")
 
 # Running Wilcoxon paired tests for each pair (below vs. above, below vs. eye, below vs. right)
 stat_above, p_above = wilcoxon(below_f, above_f)
 stat_eye, p_eye = wilcoxon(below_f, eye_f)
 stat_right, p_right = wilcoxon(below_f, right_f)
+
+# stat_stupid, p_stupid = wilcoxon(eye_f, right_f)
+# stat_above_right, p_above_right = wilcoxon(above_f, right_f)
+# stat_above_eye, p_above_eye = wilcoxon(above_f, eye_f)
 
 # Printing results with labels
 print(f"Wilcoxon test between 'below' and 'above':\nTest statistic = {stat_above}, p-value = {p_above}")
@@ -34,3 +38,13 @@ print(f"Wilcoxon test between 'below' and 'right':\nTest statistic = {stat_right
 print("Stat sig: YES\n\n" if p_right < 0.05 else "Stat sig: NO\n\n")
 
 
+
+
+# print(f"Wilcoxon test between 'eye' and 'right':\nTest statistic = {stat_stupid}, p-value = {p_stupid}")
+# print("Stat sig: YES\n\n" if p_stupid < 0.05 else "Stat sig: NO\n\n")
+
+# print(f"Wilcoxon test between 'right' and 'above':\nTest statistic = {stat_above_right}, p-value = {p_above_right}")
+# print("Stat sig: YES\n\n" if p_above_right < 0.05 else "Stat sig: NO\n\n")
+
+# print(f"Wilcoxon test between 'above' and 'eye':\nTest statistic = {stat_above_eye}, p-value = {p_above_eye}")
+# print("Stat sig: YES\n\n" if p_above_eye < 0.05 else "Stat sig: NO\n\n")
